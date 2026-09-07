9546483b22f428049719afc88a4c52bb960e747e
konm0793-web <konm0793@gmail.com>
2026-09-07 09:46:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class RateLimiter:
                 self.calls = [t for t in self.calls if now - t < self.period]
             self.calls.append(now)
 
-ivas_limiter = RateLimiter(max_calls=2, period=1.0)
+ivas_limiter = RateLimiter(max_calls=1, period=1.5)
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # WORKER POOL  (proxy fallback jika kena rate-limit)
