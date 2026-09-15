77964106e3b724ced327caf69ee27adb41793125
konm0793-web <konm0793@gmail.com>
2026-09-15 07:30:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1071,17 +1071,22 @@ def get_all_numbers_auto(acc):
         
     return list(set(all_numbers)) # Return semua nomor tanpa duplikat
     
+
 def get_active_account():
-    # 1. Cek kalau variabelnya 'accounts'
+    # 1. Cek kalau accounts berisi list of dict
     if 'accounts' in globals() and accounts:
-        return accounts[0]
-    # 2. Cek kalau variabelnya 'WORKER_POOL'
-    elif 'WORKER_POOL' in globals() and WORKER_POOL:
-        return WORKER_POOL[0]
-    # 3. Cek kalau variabelnya 'ACCOUNTS'
-    elif 'ACCOUNTS' in globals() and ACCOUNTS:
-        return ACCOUNTS[0]
+        acc = accounts[0]
+        if isinstance(acc, dict):
+            return acc
+            
+    # 2. Cek kalau WORKER_POOL berisi list of dict
+    if 'WORKER_POOL' in globals() and WORKER_POOL:
+        acc = WORKER_POOL[0]
+        if isinstance(acc, dict):
+            return acc
+
     return None
+    
     
     
 if __name__ == "__main__":
