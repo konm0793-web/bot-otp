f624e28ffd259ce358f45c2ab4a0f63c2c5874b0
konm0793-web <konm0793@gmail.com>
2026-09-23 04:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class RateLimiter:
                 self.calls = [t for t in self.calls if now - t < self.period]
             self.calls.append(now)
 
-ivas_limiter = RateLimiter(max_calls=6, period=3.0)
+ivas_limiter = RateLimiter(max_calls=5, period=3.0)
 
 def get_base():
     with _worker_lock:
